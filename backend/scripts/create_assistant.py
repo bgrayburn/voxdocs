b8baa8602e39637421d482358ab298fc4030485d
Brian Rayburn <bgrayburn@gmail.com>
2024-07-18 11:36:04 -0400

    have create_assistant script return new assistant id

--- backend/scripts/create_assistant.py
+++ backend/scripts/create_assistant.py
@@ -4,6 +4,8 @@ client = OpenAI()
 
 instructions = "You are an assistant to someone editing a markdown document.  Please always return just the documents text after making any changes the user asked for. Don't write any new text unless explicitly asked to."
 
-client.beta.assistants.create(
+res = client.beta.assistants.create(
     name="voxdocs-instruct", instructions=instructions, model="gpt-4o"
 )
+
+print(res.id)
