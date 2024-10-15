955222276008a1ad379bbd3710d0e54a4152f759
Brian Rayburn <bgrayburn@gmail.com>
2024-10-15 12:31:58 -0400

    bust cache

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { version } from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,16 @@ export default defineConfig({
       "/api": {
         target: "http://localhost:5000",
         changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        dir: "dist/client",
+        entryFileNames: `[name].${version}.js`,
+        chunkFileNames: `[name].${version}.js`,
+        assetFileNames: `[name].${version}.[ext]`,
       },
     },
   },
